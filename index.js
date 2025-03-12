bc39505d7a888c9d6494205db2e51da1ccb27308
Zain Riaz <ryzon@kali.xerik>
2025-03-12 12:11:09 +0500

    Authentication Module Addition

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb://localhost:27017/ludo_game", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("✅ MongoDB Connected"))
     .catch(err => console.error("❌ MongoDB Connection Error:", err));
 
